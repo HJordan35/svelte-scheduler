0151efaf6fcc859ba44182d9da037ceed35d51f5
Henry Jordan <henry.jordan@credera.com>
2019-07-12 17:10:54 -0500

    INITIAL | Boilerplate svelte with configuration for SCSS

---
+++ src/components/header/Header.svelte
@@ -0,0 +1,16 @@
+<script>
+  export let headline = "Headline";
+</script>
+
+<style type="text/scss">
+  .container {
+    background-color: green;
+    h1 {
+      color: white;
+    }
+  }
+</style>
+
+<div class="container">
+  <h1>{headline}</h1>
+</div>
